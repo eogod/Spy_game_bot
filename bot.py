1449f5f60c421498df6df49d3ae4b455082bc2df
Varvara <email>
2024-08-06 03:44:57 -0500

    base buttons

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import load_config
 import logging
 
@@ -8,10 +9,18 @@ bot = telebot.TeleBot(load_config())
 # Define a command handler
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Welcome to Spy game bot")
+    buttons = InlineKeyboardMarkup(row_width=1)
+    buttons.add(InlineKeyboardButton(text='invite', callback_data='invite'), InlineKeyboardButton(text='rating', callback_data='rating'))
+    bot.reply_to(message, "Welcome to Spy game bot", reply_markup=buttons)
 
 @bot.message_handler(commands=['info'])
 def send_info(message):
     bot.reply_to(message, "some text")
 
 
+@bot.callback_query_handler(func=lambda call: True)
+def answer(call):
+    if call.data == 'invite':
+        bot.send_message(call.message.chat.id, 'invite')
+    elif call.data == 'rating':
+        bot.send_message(call.message.chat.id, 'rating')
